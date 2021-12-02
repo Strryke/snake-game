64be27629fdfef971d822d0a3f6c0c8098e69bd9
Strryke <gavinong2000@gmail.com>
2021-12-02 20:54:07 +0800

    first commit

---
+++ food.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+class Food(Turtle):
+    def __init__(self, location):
+        super().__init__()
+        self.shape("square")
+        self.up()
+        self.color("red")
+        self.setposition(location)
+        
+    def new_food(self, location):
+        self.setposition(location)
+    
