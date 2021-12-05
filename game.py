a8a7bccfaa0f84d9e78bab9821fa95009f172f74
Strryke <gavinong2000@gmail.com>
2021-12-05 17:56:48 +0800

    fixed reset + window placement

--- game.py
+++ game.py
@@ -1,14 +1,18 @@
-from turtle import Turtle, Screen, screensize
+from turtle import Turtle
 from time import sleep
 from random import randrange
-import turtle
 from snake import Snake
 from food import Food, words
 from screen import Window
 
 vocab = words
-images_files={"TORTOISE":"tortoise.gif","HARE":'hare.gif',"RACE":'race.gif',"FINAL":'final.gif'}
-completedls=[]
+images_files = {
+    "TORTOISE": "tortoise.gif",
+    "HARE": "hare.gif",
+    "RACE": "race.gif",
+    "FINAL": "final.gif",
+}
+completedls = []
 wordsindex = 0
 charindex = 0
 style = ("Verdana", 30)
@@ -18,27 +22,11 @@ playing = True
 def top_spelling(word, charindex):
     spelling = Turtle()
     spelling.up()
-    spelling.setpos(0, 400)
+    spelling.setpos(0, 380)
     spelling.color("white")
     spelling.hideturtle()
     spelling.write(word[0:charindex], align="center", font=style)
     return spelling
-
-
-def word_list(x,ls):
-    for i in range(len(ls)):
-        completedword = Turtle()
-        completedword.setpos(x, -(i * 40))
-        completedword.color("yellow")
-        completedword.hideturtle()
-        completedword.write(ls[i], align="left", font=style)
-
-def show_image(word):
-    image=Turtle()
-    image.up()
-    image.setpos(-500,200)
-    window.screen.addshape(images_files[word])
-    image.shape(images_files[word])
 
 
 # Returns a location away from the Snake
@@ -56,39 +44,35 @@ def eat_food(head, food):
         return True
 
 
-def endgame(completedls,msg):
-    window.reset_screen()
-    word_list(0,completedls)
-
-    show_image("FINAL") #not working :(
-
+def endgame(completedls, msg):
+    switch_playstate()
+    # window.show_image(images_files["FINAL"])
+    # window.image.setpos(0, 0)
     text = Turtle()
+    text.up()
     text.setpos(0, 200)
     text.hideturtle()
-    text.color("white")
-    text.write(f"{msg}\nClick anywhere to exit", align="center", font=style)
-    switch_playstate()
-    #window.screen.onkey(restart, "r")
-    #window.screen.listen()
-
-    window.screen.exitonclick()
+    text.color("White")
+    text.write(f"{msg}\nPress 'R' to reset the game", align="center", font=style)
 
 
 def switch_playstate():
     global playing
     playing = not playing
 
-'''
+
 def restart():
     global playing, food, charindex
-    window.reset()
+    window.reset_screen()
     snake.__init__()
     playing = True
     charindex = 0
     key_binds()
     food = Food(generate_rand(), word[0])
     window.bottom_text()
-'''
+    window.show_image(images_files[word])
+    window.update()
+
 
 def key_binds():
     window.screen.onkey(snake.up, "Up")
@@ -96,7 +80,7 @@ def key_binds():
     window.screen.onkey(snake.left, "Left")
     window.screen.onkey(snake.right, "Right")
     window.screen.onkey(switch_playstate, "space")
-    #window.screen.onkey(restart, "r")
+    window.screen.onkey(restart, "r")
 
 
 window = Window()
@@ -121,9 +105,10 @@ while True:
 snake = Snake()
 window.edge_drawing()
 window.bottom_text()
+window.word_list([])
 
 word = vocab[0]
-show_image(word)
+window.show_image(images_files[word])
 spelling = top_spelling(word, 0)
 
 food = Food(generate_rand(), word[0])
@@ -138,22 +123,23 @@ while True:
         snake.move()
 
         if snake.collide():
-            endgame(completedls,"GAME OVER")
+            endgame(completedls, "GAME OVER")
 
         elif eat_food(snake.head, food):
             snake.eat()  # creates new segments
-        
+
             if charindex == len(word) - 1:
                 completedls.append(word)
-                if len(completedls)==len(vocab):
-                    endgame(completedls,"CONGRATULATIONS! YOU WIN!")
+                if len(completedls) == len(vocab):
+                    endgame(completedls, "CONGRATULATIONS! YOU WIN!")
                 else:
-                    word_list(-400,completedls)
+                    window.word_list(completedls)
                     wordsindex += 1
                     word = vocab[wordsindex]
                     charindex = 0
                     food.new_food(generate_rand(), word[charindex])
-                    show_image(word)
+                    window.del_image()
+                    window.show_image(images_files[word])
 
             else:
                 charindex += 1
